623565eb7fab34efc2ab17e6a248b95f0da97e01
amine <amine.tamouh.officiel@gmail.com>
2022-01-25 13:18:30 +0100

    'all_fet_compl'

--- todo.js
+++ todo.js
@@ -6,7 +6,10 @@ function add(){
     var task = document.getElementById("ctn").textContent.trim()
    if(task!=""){
     document.getElementById("ulg").innerHTML+='<li class="list-group-item"><span>'+task+'</span><div class="icons"><i class="fas fa-trash" onclick="deleteTask()" title="delete"></i><i class="fas fa-user-edit" title="edit"></i></div></li>'
-   }
+    document.getElementById("ctn").textContent=""
+    }else{
+        alert("Task not avalaible")
+    }
 }
 
 /* ---------------------------------- Supprimer  ---------------------------------- */
@@ -49,5 +52,18 @@ function editTask(){
     }
 }
 
+/* ---------------------------------- Chercher tache  ---------------------------------- */
 
+document.getElementById("search").addEventListener("keyup",chercher)
+function chercher(){
+    var cle = this.value
+    for(var i = 0; i < list.length; i++) {
+        if(!list[i].children[0].textContent.trim().toUpperCase().includes(cle.toUpperCase())){
+            list[i].style.display="none"
+        }else{
+            list[i].style.display="block"
+        }
+        
+    }
+}
 
